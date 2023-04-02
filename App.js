5a776e0d984a6747551320d7986d79644c0896c4
Vijay Sundar <112871288+sucvijay@users.noreply.github.com>
2023-04-02 16:50:17 +0530

    1

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Modal, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const ip = "http://192.168.43.75:3000/openai/"
+
+  
+
+  const [img,setimg] = useState(ip+"iron");
+  const [txt,settxt] = useState(ip);
+
+  const [ld,setld] = useState(false);
+
+
+
+
+
+  const searchbtn = () =>{
+
+    setimg(ip+txt)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Modal visible={ld} >
+      <View style={{alignItems:'center',justifyContent:'center',width:'100%',height:'100%'}}>
+        <Image source={require("../gd/assets/lding.gif")} style={{alignItems:'center',justifyContent:'center'}} />
+
+      </View>
+      </Modal>
+      <Image source={require("./assets/bg.jpg")} resizeMode="cover" style={styles.bg}/>
+      <View style={styles.header}>
+      <Image source={require("./assets/img.png")} style={styles.cam} />
+      </View>
+
+      <View style={[styles.header,{justifyContent:'space-evenly',height:'auto'}]}>
+      <TextInput placeholder='UnderWater City' style={styles.searchbar} onChangeText={text=> settxt(text) } />
+      <TouchableOpacity onPress={searchbtn}>
+      <Image source={require("./assets/search.png")} style={styles.searchbtn}  />
+      </TouchableOpacity>
+      </View>
+
+
+
+      <View style={styles.otp}>
+      <Image source={require("../gd/assets/lding.gif")} style={{alignItems:'center',justifyContent:'center',width:100,height:100,position:'absolute',alignSelf:'center'}} />
+      <Image style={{flex:1,borderRadius:15}} source={{uri:img}} resizeMode="cover" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A4335E',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  bg:{
+    width:"100%",
+    height:'100%',
+    position:'absolute'
+  },
+  header:{
+    width:'100%',
+    height:100,
+    flexDirection:'row'
+  },
+  searchbar:{
+    width:'80%',
+    height:50,
+    backgroundColor:"#8E7B72",
+    borderRadius:20,
+    borderWidth:5,
+    borderColor:'black',
+    color:'white',
+    paddingLeft:50
+  },
+  cam:{
+    width:80,
+    height:80,
+  },
+  otp:{
+    width:"98%",
+    height:"55%",
+    borderColor:'#8E7B72',
+    marginTop:30,
+    borderRadius:20,
+    borderWidth:5,
+    backgroundColor:'white'
+  },
+  searchbtn:{
+    width:50,
+    height:50,
+    backgroundColor:'white',
+    borderRadius:50,
+    elevation:5,
+    borderWidth:3,
+    borderColor:'black'
+  }
 });
